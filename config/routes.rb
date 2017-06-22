2377483a396ce742591c1f6eb2a2c7640abbe4be
Dom <dominik.reller@gmail.com>
2017-06-21 21:38:47 -0300

    Criado controller para SubCategory e painel administrativo. Criado rotas e index.html do painel administrativo.
    As rotas criadas seguem a especificação pedida na descrição do teste.

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,20 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # Normal routing
+  # resources :categories do
+  #   resources :sub_categories
+  # end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/admin', to: 'admin_panel#index'
+  get '/admin/:slug', :as => :edit_sub_category,
+                      :via => :get,
+                      :controller => :sub_categories,
+                      :action => :edit
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    # Requested routing as defined on the README.rdoc.
+    # NOTE: I didn't know if I should keep them in english or portuguese, so I keep them "following" how :id normally works
+  match ':category_slug/:slug',  :as => :category_sub_category,
+                        :via => :get,
+                        :controller => :sub_categories,
+                        :action => :show
 end
